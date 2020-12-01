38dfc561aca8b4b0ed300a3ae5765afb78d42924
HeadTwerk <HeadTwerk@github.com>
2020-12-01 12:17:23 +0530

    setting up server 7

--- test_project/landing_page.js
+++ test_project/landing_page.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile('./landing-page/home.html', { root: __dirname });
+    res.sendFile('./home.html', { root: __dirname });
 });
 app.get('/about', (req, res) => {
-    res.sendFile('./landing-page/about.html', { root: __dirname });
+    res.sendFile('./about.html', { root: __dirname });
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile('./landing-page/contact.html', { root: __dirname });
+    res.sendFile('./contact.html', { root: __dirname });
 });
     
 app.listen(port, () => console.log(`listening on port ${port}!`));
