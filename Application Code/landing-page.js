673ad579884c9c7258c24daae7e4537d56b96b89
HeadTwerk <HeadTwerk@github.com>
2020-12-01 11:12:52 +0530

    setting up server 4

--- Application Code/landing-page.js
+++ Application Code/landing-page.js
@@ -1,12 +1,12 @@
-const express= require('express');      //Importing express library
-const app= express();                   //Creating the express app
-const port= 8080;                       //port to listen on
+const express = require('express');      //Importing express library
+const app = express();                //Creating the express app
+const port = 3000;                      //port to listen on
 
 //To Serve files as requested by address 
 app.get('/', (req, res) => {
-    res.sendFile('./public/home.html', { root: __dirname });
+    res.sendFile('./landing-page/home.html', { root: __dirname });
 });
 
 app.get('/webtest', (req, res) => {
-    res.sendFile('./public/webtest.html', { root: __dirname });
-}); 
+    res.sendFile('./landing-page/webtest.html', { root: __dirname });
+});     
